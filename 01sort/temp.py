14bbc5531e96bae646ad1f56bfc6e11f4c4f4926
dinghing <dinghing@dinghingmatoMacBook-Pro.local>
2018-10-24 08:12:46 +0900

    add a new file about the meetup

---
+++ 01sort/temp.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import meetup.api as mp
+import pprint
+
+client = mp.Client()
+client.api_key = "35b934503097868954364815526f"
+
+#category = client.GetCategories()
+#print(category.results)
+
+#member = client.GetMembers(group_id="1474611")
+#print(member.__dict__.keys())
+#pprint.pprint(member.results)
+
+#m_ex=client.GetMember(id=77544882)
+print(1540418400000/3600)
